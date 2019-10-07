900dd67a75b42ca964afa9b275341295e8cd5b85
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-10-07 22:00:53 +1100

    Created a CollectionPage that is a duplicate of Items Page including List view, this will be replaced but leaving ListView in place
    Cleaned up some alignment to make it easier to compare the two

--- CollectionViewUpgrade/ViewModels/CollectionViewModel.cs
+++ CollectionViewUpgrade/ViewModels/CollectionViewModel.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using CollectionViewUpgrade.Models;
 using CollectionViewUpgrade.ViewModels;
+using CollectionViewUpgrade.Views;
+using Xamarin.Forms;
 
 namespace CollectionViewUpgrade.ViewModels
 {
     public class CollectionViewModel : BaseViewModel
     {
+        public ObservableCollection<Item> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+
+        public CollectionViewModel()
+        {
+            Title = "Browse";
+            Items = new ObservableCollection<Item>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+            MessagingCenter.Subscribe<NewItemPage, Item>(this, "AddItem", async (obj, item) =>
+            {
+                var newItem = item as Item;
+                Items.Add(newItem);
+                await DataStore.AddItemAsync(newItem);
+            });
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
     }
 }
--- CollectionViewUpgrade/Views/CollectionPage.xaml.cs
+++ CollectionViewUpgrade/Views/CollectionPage.xaml.cs
@@ -13,5 +13,29 @@ namespace CollectionViewUpgrade.Views
             BindingContext = new CollectionViewModel();
             InitializeComponent();
         }
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Item;
+            if (item == null)
+                return;
+
+            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
     }
 }
