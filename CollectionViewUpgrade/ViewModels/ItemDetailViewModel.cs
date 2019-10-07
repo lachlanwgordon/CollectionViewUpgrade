86b58043ea8854d49c5cba689a4fc2fd84595c20
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-10-07 21:46:20 +1100

    File>New>Shell

---
+++ CollectionViewUpgrade/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CollectionViewUpgrade.Models;
+
+namespace CollectionViewUpgrade.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
