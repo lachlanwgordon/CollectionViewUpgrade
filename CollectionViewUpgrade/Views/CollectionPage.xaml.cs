15109e40189f51211c98a65b449dc2c4ee4a14f7
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-10-07 22:00:27 +1100

    Created a CollectionPage that is a duplicate of Items Page including List view, this will be replaced but leaving ListView in place
    Cleaned up some alignment to make it easier to compare the two

---
+++ CollectionViewUpgrade/Views/CollectionPage.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using CollectionViewUpgrade.ViewModels;
+using Xamarin.Forms;
+
+
+namespace CollectionViewUpgrade.Views
+{
+    public partial class CollectionPage : ContentPage
+    {
+        public CollectionViewModel ViewModel => BindingContext as CollectionViewModel;
+        public CollectionPage()
+        {
+            BindingContext = new CollectionViewModel();
+            InitializeComponent();
+        }
+    }
+}
