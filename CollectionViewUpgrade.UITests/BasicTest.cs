bdfc39f3e056cb9bcb329c6893ad81425c7c33ad
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-10-08 11:35:03 +1100

    Changed mind about having the extra page.
    Wrote UITest for Visitng All Pages and For adding an Item

---
+++ CollectionViewUpgrade.UITests/BasicTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace CollectionViewUpgrade.UITests
+{
+    public class BasicTest
+    {
+        [TestFixture(Platform.Android)]
+        [TestFixture(Platform.iOS)]
+        public class Tests
+        {
+            IApp app;
+            Platform platform;
+
+            public Tests(Platform platform)
+            {
+                this.platform = platform;
+            }
+
+            [SetUp]
+            public void BeforeEachTest()
+            {
+                app = AppInitializer.StartApp(platform);
+            }
+
+            [Test]
+            public void VisitAllPages()
+            {
+                app.Tap("About");
+                app.WaitForElement(x => x.Marked("Learn more"));
+
+                app.Tap("List");
+                app.WaitForElement(x => x.Marked("Add"));
+
+                app.Tap("Add");
+                app.WaitForElement(x => x.Marked("Cancel"));
+
+                app.Tap("Cancel");
+                app.WaitForElement(x => x.Marked("ItemsList"));
+                //app.Repl();
+                app.Tap(x => x.Marked("Third item"));
+                app.WaitForElement(x => x.Marked("Description:"));
+                app.Back();
+                Assert.Pass();
+            }
+
+            [Test]
+            public void AddItem()
+            {
+                //app.Repl();
+                app.WaitForElement("Add");
+                var itemsAtStart = app.Query(x => x.Marked("ItemsList").Child());
+                app.Tap("Add");
+                //app.Repl();
+
+                app.Tap("NameEntry");
+                app.ClearText();
+                app.EnterText("Test item2");
+                app.DismissKeyboard();
+
+                app.Tap("DescriptionEditor");
+                app.ClearText();
+                var newGuid = Guid.NewGuid();
+                app.EnterText($"New item {newGuid}");
+                app.Tap("Save");
+                app.WaitForElement("Test item2");
+
+                var itemsAfterAdding = app.Query(x => x.Marked("ItemsList").Child());
+                var item = app.Query(x => x.Marked($"New item {newGuid}"));
+
+                Assert.Greater(item.Length, 0);
+
+            }
+
+        }
+    }
+}
